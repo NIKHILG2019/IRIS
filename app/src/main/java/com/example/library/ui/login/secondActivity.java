125d258cc634ca85d4f64c215300f0f2b6ff1140
Harshita-Kanal <harshita.kgv@gmail.com>
2020-03-03 16:25:25 +0530

    file

---
+++ app/src/main/java/com/example/library/ui/login/secondActivity.java
@@ -0,0 +1,34 @@
+package com.example.library.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.library.R;
+
+public class secondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void second(View view) {
+
+    }
+
+    public void third(View view) {
+    }
+
+    public void fourth(View view) {
+    }
+
+    public void fifth(View view) {
+    }
+
+    public void sixth(View view) {
+    }
+}
